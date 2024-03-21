a8492ca1fff56aa0cf827e9a04c7429ba0d7c380
Андрей Щиголь <andryushka05@gmail.com>
2024-03-21 18:27:25 +0200

    Added handler.go

--- handler.go
+++ handler.go
@@ -1,12 +1,50 @@
 package lab2
 
-// ComputeHandler should be constructed with input io.Reader and output io.Writer.
-// Its Compute() method should read the expression from input and write the computed result to the output.
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"strings"
+)
+
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	input  string
+	output io.Writer
+}
+
+func NewComputeHandler(input string, output io.Writer) *ComputeHandler {
+	return &ComputeHandler{input: input, output: output}
 }
 
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
+	expression := strings.Fields(ch.input)
+	if len(expression) == 0 {
+		return errors.New("відсутні дані для обробки")
+	}
+
+	result, err := PostfixToInfix(strings.Join(expression, " "))
+	if err != nil {
+		return err
+	}
+
+	_, err = ch.output.Write([]byte(result))
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func ParseInput(expressionFlag, fileFlag string) (string, error) {
+	if expressionFlag != "" {
+		return expressionFlag, nil
+	} else if fileFlag != "" {
+		data, err := ioutil.ReadFile(fileFlag)
+		if err != nil {
+			return "", err
+		}
+		return string(data), nil
+	} else {
+		return "", errors.New("не вказаний вираз або файл з виразом")
+	}
 }
